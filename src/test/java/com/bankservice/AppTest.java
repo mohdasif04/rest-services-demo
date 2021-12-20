ade3b8b790802c6ac59d7da2e3ac370128ccd31e
asif <mohammadasif0488@gmail.com>
2021-12-20 15:45:40 +0530

    "First Commit"

---
+++ src/test/java/com/bankservice/AppTest.java
@@ -0,0 +1,20 @@
+package com.bankservice;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
